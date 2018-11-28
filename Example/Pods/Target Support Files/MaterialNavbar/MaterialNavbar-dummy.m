b32097b378f6bb1c76a3341ba38e962e3ed08484
Jogendra Singh <imjog24@gmail.com>
2018-11-28 13:13:43 +0530

    Added Example and Static

---
+++ Example/Pods/Target Support Files/MaterialNavbar/MaterialNavbar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MaterialNavbar : NSObject
+@end
+@implementation PodsDummy_MaterialNavbar
+@end
---
+++ Example/Pods/Target Support Files/Pods-MaterialNavbar/Pods-MaterialNavbar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MaterialNavbar : NSObject
+@end
+@implementation PodsDummy_Pods_MaterialNavbar
+@end
